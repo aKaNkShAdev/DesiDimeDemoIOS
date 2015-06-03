5c7f7709b8a66c98536ac1ee1b32ab0dcd7b1821
Akanksha Sharma <akankshasharma@Akankshas-Mac-mini.local>
2015-06-03 20:41:36 +0530

    Initial layout

---
+++ DesidimeDemo/DesidimeDemo/DealCellTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  DealCellTableViewCell.swift
+//  DesidimeDemo
+//
+//  Created by Akanksha Sharma on 03/06/15.
+//  Copyright (c) 2015 akanksha. All rights reserved.
+//
+
+import UIKit
+
+class DealCellTableViewCell: UITableViewCell {
+    @IBOutlet weak var dealDescription: UILabel!
+    @IBOutlet weak var dealTitle: UILabel!
+    @IBOutlet weak var dealImage: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- DesidimeDemo/DesidimeDemo/ViewController.swift
+++ DesidimeDemo/DesidimeDemo/ViewController.swift
@@ -8,18 +8,78 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
+    //IBOutlets
+    @IBOutlet weak var dealTable: UITableView!
+    @IBOutlet weak var popularTable: UITableView!
+    @IBOutlet var popularTableWidth: [NSLayoutConstraint]!
+    @IBOutlet weak var mainView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var leftSwipe = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipes:"))
+        var rightSwipe = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipes:"))
+        
+        leftSwipe.direction = .Left
+        rightSwipe.direction = .Right
+        
+        view.addGestureRecognizer(leftSwipe)
+        view.addGestureRecognizer(rightSwipe)
+        
     }
 
+    override func viewDidLayoutSubviews() {
+        popularTable.center = CGPointMake(popularTable.center.x + popularTable.frame.width, popularTable.center.y)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    // TABLEVIEW DELEGATE METHODS
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        var cell : DealCellTableViewCell = tableView.dequeueReusableCellWithIdentifier("DealCell", forIndexPath: indexPath) as! DealCellTableViewCell
+        return cell;
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat{
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat{
+        return 5
+    }
+    
+    func handleSwipes(sender:UISwipeGestureRecognizer) {
+        if (sender.direction == .Left) {
+            UIView.animateWithDuration(0.5 as NSTimeInterval, animations: {
+                self.mainView.center = CGPointMake(self.mainView.center.x - self.mainView.frame.width, self.mainView.center.y)
 
+                }, completion: {
+                    finished in
+            })
+        }
+        
+        if (sender.direction == .Right) {
+            UIView.animateWithDuration(0.5 as NSTimeInterval, animations: {
+                self.mainView.center = CGPointMake(self.mainView.center.x + self.mainView.frame.width, self.mainView.center.y)
+                }, completion: {
+                    finished in
+            })
+        }
+    }
 
 }
 
